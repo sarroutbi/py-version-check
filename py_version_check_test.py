a2115b62d06b1c63f0208b3e5b4065f9bc3a43cf
Sergio Arroutbi <sarroutb@redhat.com>
2024-04-19 18:28:54 +0200

    Pad versions in python easily (#1)
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ py_version_check_test.py
@@ -0,0 +1,26 @@
+import unittest
+
+from py_version_check import minor_version
+
+class TestVersions(unittest.TestCase):
+
+    def test_minor_normal(self):
+        self.assertEqual(minor_version("1.0", "1.1"), True)
+
+    def test_minor_normal2(self):
+        self.assertEqual(minor_version("0.1", "1.1"), True)
+
+    def test_minor_normal3(self):
+        self.assertEqual(minor_version("0.1.0", "1.1.0"), True)
+
+    def test_minor_tricky1(self):
+        self.assertEqual(minor_version("01.0", "010.0"), True)
+
+    def test_minor_tricky2(self):
+        self.assertEqual(minor_version("01.0", "0.010.0"), False)
+
+    def test_minor_tricky3(self):
+        self.assertEqual(minor_version("01.0", "00002.010.0"), True)
+
+if __name__ == '__main__':
+    unittest.main()
